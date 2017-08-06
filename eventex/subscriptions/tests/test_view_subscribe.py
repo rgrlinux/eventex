c07b7a348110883b90a2b60c5eb1b662eccb67e4
rogerio oliveira <rgrlinux@gmail.com>
2017-08-06 15:04:38 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -38,6 +38,23 @@ class SubscribeGet(TestCase):
         self.assertIsInstance(form, SubscriptionForm)
 
 
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Rogerio Oliveira', cpf='31095929836',
+                    email='rgrlinux@gmail.com', phone='11-98088-7373')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
+
+
 class SubscribePostInvalid(TestCase):
     def setUp(self):
         self.resp = self.client.post('/inscricao/', {})
@@ -60,27 +77,3 @@ class SubscribePostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
-
-class SubscribePostValid(TestCase):
-    def setUp(self):
-        data = dict(name='Rogerio Oliveira', cpf='31095929836',
-                    email='rgrlinux@gmail.com', phone='11-98088-7373')
-        self.resp = self.client.post('/inscricao/', data)
-
-    def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
-
-    def test_send_subscribe_email(self):
-        self.assertEqual(1, len(mail.outbox))
-
-    def test_save_subscription(self):
-        self.assertTrue(Subscription.objects.exists())
-
-
-class SubscribeSuccesMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Rogerio Oliveira', cpf='31095929836',
-                    email='rgrlinux@gmail.com', phone='11-98088-7373')
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
